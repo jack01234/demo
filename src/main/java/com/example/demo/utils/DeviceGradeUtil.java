beba7e013db1d299c1dccc69e995dc31d917a3fd
liwenming <yingmuhuadao@baofu.com>
2018-08-31 17:44:59 +0800

    修改

--- src/main/java/com/example/demo/utils/DeviceGradeUtil.java
+++ src/main/java/com/example/demo/utils/DeviceGradeUtil.java
@@ -9,7 +9,9 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 import static com.example.demo.common.CommonConstant.DEVICE_SCORE;
 import static com.example.demo.common.CommonConstant.PC_DEVICE_SCORE;
@@ -35,72 +37,72 @@ public class DeviceGradeUtil {
     static {
         log.info("初始化特征分数开始");
 
-        /**非IE8浏览器JS特征分数**/
+        /**非IE8浏览器JS特征分数5.377**/
 
-        pcMap.put(USER_AGENT, BigDecimal.valueOf(0.5));
-        pcMap.put(EVER_COOKIE,BigDecimal.valueOf(1.5));
-        pcMap.put(EXTEND1,BigDecimal.valueOf(1.5));
+        pcMap.put(USER_AGENT, BigDecimal.valueOf(0.6));
+        pcMap.put(EVER_COOKIE,BigDecimal.valueOf(1.843));
+        pcMap.put(EXTEND1,BigDecimal.valueOf(1.843));
         pcMap.put(LANGUAGE, BigDecimal.valueOf(0.344));
         pcMap.put(DEVICE_MEMORY,BigDecimal.valueOf(0.37));
-        pcMap.put(RESOLUTION,BigDecimal.valueOf(0.263));
+        pcMap.put(RESOLUTION,BigDecimal.valueOf(0.25));
         pcMap.put(TIME_ZONE,BigDecimal.valueOf(0.198));
         pcMap.put(SESSION_STORAGE,BigDecimal.valueOf(0.024));
         pcMap.put(LOCAL_STORAGE,BigDecimal.valueOf(0.024));
         pcMap.put(OPEN_DATABASE,BigDecimal.valueOf(0.024));
         pcMap.put(HASHINDEX_DB,BigDecimal.valueOf(0.024));
-        pcMap.put(PLUGINS,BigDecimal.valueOf(0.43));
-        pcMap.put(CANVAS_ID,BigDecimal.valueOf(0.35));
+        pcMap.put(PLUGINS,BigDecimal.valueOf(0.2));
+        pcMap.put(CANVAS_ID,BigDecimal.valueOf(0.3));
         pcMap.put(WEBGL_RENDERER,BigDecimal.valueOf(0.2));
         pcMap.put(AD_BLOCK,BigDecimal.valueOf(0.056));
         pcMap.put(JS_FONTS,BigDecimal.valueOf(0.42));
         pcMap.put(AUDIO_ID,BigDecimal.valueOf(0.2));
-        pcMap.put(VIDEO_ID,BigDecimal.valueOf(0.45));
+        pcMap.put(VIDEO_ID,BigDecimal.valueOf(0.3));
 
-        /**IE8浏览器JS特征分数**/
+        /**IE8浏览器JS特征分数4.859**/
 
-        iePcMap.put(USER_AGENT, BigDecimal.valueOf(0.48));
-        iePcMap.put(EVER_COOKIE,BigDecimal.valueOf(2));
-        iePcMap.put(EXTEND1,BigDecimal.valueOf(2));
+        iePcMap.put(USER_AGENT, BigDecimal.valueOf(0.5));
+        iePcMap.put(EVER_COOKIE,BigDecimal.valueOf(2.223));
+        iePcMap.put(EXTEND1,BigDecimal.valueOf(2.223));
         iePcMap.put(LANGUAGE, BigDecimal.valueOf(0.344));
         iePcMap.put(DEVICE_MEMORY,BigDecimal.valueOf(0.37));
-        iePcMap.put(RESOLUTION,BigDecimal.valueOf(0.263));
+        iePcMap.put(RESOLUTION,BigDecimal.valueOf(0.25));
         iePcMap.put(TIME_ZONE,BigDecimal.valueOf(0.198));
         iePcMap.put(SESSION_STORAGE,BigDecimal.valueOf(0.024));
         iePcMap.put(LOCAL_STORAGE,BigDecimal.valueOf(0.024));
         iePcMap.put(OPEN_DATABASE,BigDecimal.valueOf(0.024));
         iePcMap.put(HASHINDEX_DB,BigDecimal.valueOf(0.024));
-        iePcMap.put(PLUGINS,BigDecimal.valueOf(0.43));
+        iePcMap.put(PLUGINS,BigDecimal.valueOf(0.2));
         iePcMap.put(WEBGL_RENDERER,BigDecimal.valueOf(0.202));
         iePcMap.put(AD_BLOCK,BigDecimal.valueOf(0.056));
         iePcMap.put(JS_FONTS,BigDecimal.valueOf(0.42));
 
 
-        /**非IE8浏览器H5特征分数**/
+        /**非IE8浏览器H5特征分数4.018**/
 
-        clientMap.put(USER_AGENT,BigDecimal.valueOf(0.741));
-        clientMap.put(EVER_COOKIE,BigDecimal.valueOf(0.74));
-        clientMap.put(EXTEND1,BigDecimal.valueOf(0.74));
+        clientMap.put(USER_AGENT,BigDecimal.valueOf(0.5));
+        clientMap.put(EVER_COOKIE,BigDecimal.valueOf(1.517));
+        clientMap.put(EXTEND1,BigDecimal.valueOf(1.517));
         clientMap.put(LANGUAGE,BigDecimal.valueOf(0.32));
         clientMap.put(DEVICE_MEMORY,BigDecimal.valueOf(0.35));
-        clientMap.put(RESOLUTION,BigDecimal.valueOf(0.366));
+        clientMap.put(RESOLUTION,BigDecimal.valueOf(0.2));
         clientMap.put(TIME_ZONE,BigDecimal.valueOf(0.245));
         clientMap.put(SESSION_STORAGE,BigDecimal.valueOf(0.037));
         clientMap.put(LOCAL_STORAGE,BigDecimal.valueOf(0.037));
         clientMap.put(OPEN_DATABASE,BigDecimal.valueOf(0.037));
         clientMap.put(HASHINDEX_DB,BigDecimal.valueOf(0.037));
         clientMap.put(PLUGINS,BigDecimal.valueOf(0.081));
-        clientMap.put(CANVAS_ID,BigDecimal.valueOf(0.4));
-        clientMap.put(WEBGL_RENDERER,BigDecimal.valueOf(0.165));
+        clientMap.put(CANVAS_ID,BigDecimal.valueOf(0.25));
+        clientMap.put(WEBGL_RENDERER,BigDecimal.valueOf(0.145));
         clientMap.put(AD_BLOCK,BigDecimal.valueOf(0.029));
         clientMap.put(JS_FONTS,BigDecimal.valueOf(0.033));
-        clientMap.put(VIDEO_ID,BigDecimal.valueOf(0.4));
+        clientMap.put(VIDEO_ID,BigDecimal.valueOf(0.2));
 
 
-        /**IE8浏览器H5特征分数**/
+        /**IE8浏览器H5特征分数7.582**/
 
-        ieClientMap.put(USER_AGENT,BigDecimal.valueOf(0.741));
-        ieClientMap.put(EVER_COOKIE,BigDecimal.valueOf(5));
-        ieClientMap.put(EXTEND1,BigDecimal.valueOf(5));
+        ieClientMap.put(USER_AGENT,BigDecimal.valueOf(0.841));
+        ieClientMap.put(EVER_COOKIE,BigDecimal.valueOf(4.9));
+        ieClientMap.put(EXTEND1,BigDecimal.valueOf(4.9));
         ieClientMap.put(LANGUAGE,BigDecimal.valueOf(0.424));
         ieClientMap.put(DEVICE_MEMORY,BigDecimal.valueOf(0.35));
         ieClientMap.put(RESOLUTION,BigDecimal.valueOf(0.366));
@@ -197,5 +199,34 @@ public class DeviceGradeUtil {
         }else {
             res.setResult("新设备");
         }
+    }
+    public static void getScore(Map<String, BigDecimal> weight){
+        Set<String> strings = weight.keySet();
+        Iterator<String> iterator = strings.iterator();
+        BigDecimal score = new BigDecimal(0.0);
+        while (iterator.hasNext()){
+            String key = iterator.next();
+            if ("extend1".equals(key)) {
+                continue;
+            }
+            BigDecimal value = weight.get(key);
+            score = score.add(value);
+        }
+
+        System.out.println(score);
+    }
+
+    public static void main(String[] args) {
+
+
+        getScore(pcMap);
+        getScore(clientMap);
+        getScore(iePcMap);
+        getScore(ieClientMap);
+
+
+        System.out.println("20180831103355597941505462619201b2ba949220e2994b87f7e95cff1f78d3".hashCode());
+
+        System.out.println("20180830095607696785779603311143733ae0f05b95c7c6e6a3e53f5d77b03c".hashCode());
     }
 }
